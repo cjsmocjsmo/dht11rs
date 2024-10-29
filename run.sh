ceda85551a516a51a028e0c89898dfed5aadd4ec
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-10-29 04:13:11 -0700

    Refactor run.sh to update the path for copying the dht11res binary and handle database path

--- run.sh
+++ run.sh
@@ -7,25 +7,27 @@ current_dir=$(pwd)
 
 echo $current_dir
 
-# if [ $current_dir === "/usr/share/dht11res/dht11rs" ]; then
-#     git pull;
-#     cargo build --release;
-#     sudo cp -pvr /usr/share/dht11res/dht11res/target/release/dht11res /usr/bin/dht11res;
-#     rm /usr/share/dht11res/dht11rs/sensor_data.db;
-#     touch /usr/share/dht11res/dht11rs/sensor_data.db;
-#     # sudo systemctl daemon-reload;
-#     # sudo systemctl start dht11rs.service;
-# else
-#     cd /usr/share/dht11res;
-#     git clone https://github.com/cjsmocjsmo/dht11rs.git;
-#     cd dht11rs;
-#     cargo build --release;
-#     sudo cp -pvr /usr/share/dht11res/dht11rs/target/release/dht11res /usr/bin/dht11res;
-#     rm /usr/share/dht11res/dht11rs/sensor_data.db;
-#     touch /usr/share/dht11res/dht11rs/sensor_data.db;
-#     # sudo systemctl daemon-reload;
-#     # sudo systemctl start dht11rs.service;
-# fi
+if [ $current_dir == "/usr/share/dht11res/dht11rs" ]; then
+    git pull;
+    cargo build --release;
+    sudo cp -pvr /usr/share/dht11res/dht11res/target/release/dht11res /usr/bin/dht11res;
+    rm /usr/share/dht11res/dht11rs/sensor_data.db;
+    touch /usr/share/dht11res/dht11rs/sensor_data.db;
+    # sudo systemctl daemon-reload;
+    # sudo systemctl start dht11rs.service;
+fi
+
+if [ $current_dir == "/usr/share/dht11res" ]; then
+    cd /usr/share/dht11res;
+    git clone https://github.com/cjsmocjsmo/dht11rs.git;
+    cd dht11rs;
+    cargo build --release;
+    sudo cp -pvr /usr/share/dht11res/dht11rs/target/release/dht11res /usr/bin/dht11res;
+    rm /usr/share/dht11res/dht11rs/sensor_data.db;
+    touch /usr/share/dht11res/dht11rs/sensor_data.db;
+    # sudo systemctl daemon-reload;
+    # sudo systemctl start dht11rs.service;
+fi
 
 
 
