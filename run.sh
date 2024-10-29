fdc533e58768e0e5eb86c46a67dcc2679b00b06f
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-10-29 03:54:42 -0700

    Refactor run.sh to update the path for copying the dht11res binary

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@
     cd /usr/share/dht11res/dht11res;
     git pull;
     cargo build --release;
-    sudo cp -pvr ./target/release/dht11res /usr/bin/dht11res;
+    sudo cp -pvr /usr/share/dht11res/dht11res/target/release/dht11res /usr/bin/dht11res;
     rm ./sensor_data.db;
     touch ./sensor_data.db;
     # sudo systemctl daemon-reload;
