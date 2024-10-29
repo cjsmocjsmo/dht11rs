390dd18a05055aa8396f7140af391dcd3d68873f
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-10-29 03:51:25 -0700

    Refactor run.sh to comment out unnecessary code

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 #! /bin/sh
 
-if [ -d /usr/systemd/system/dht11rs.service ]; then
-    sudo systemctl stop dht11rs.service;
-fi
+# if [ -d /usr/systemd/system/dht11rs.service ]; then
+#     sudo systemctl stop dht11rs.service;
+# fi
 
 if [ -d /usr/share/dht11res/dht11res ]; then
     cd /usr/share/dht11res/dht11res;
     git pull;
     cargo build --release;
     sudo cp -pvr ./target/release/dht11res /usr/bin/dht11res;
-    sudo systemctl daemon-reload;
-    sudo systemctl start dht11rs.service;
+    rm ./sensor_data.db;
+    touch ./sensor_data.db;
+    # sudo systemctl daemon-reload;
+    # sudo systemctl start dht11rs.service;
 fi
