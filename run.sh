74aeb67884afaf55c5f60ef5257860ee1f3d5a8d
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-11-04 01:09:07 -0800

    Update run.sh to streamline service management and database handling

--- run.sh
+++ run.sh
@@ -1,16 +1,14 @@
 #! /bin/sh
 
-# if [ -d /usr/systemd/system/dht11rs.service ]; then
-#     sudo systemctl stop dht11rs.service;
-# fi
-
+sudo systemctl stop dht11rs.service;
+cd /usr/share/dht11res/dht11res;
 git pull;
 cargo build --release;
 sudo cp -pvr /usr/share/dht11res/dht11res/target/release/dht11res /usr/bin/dht11res;
 rm /usr/share/dht11res/dht11rs/sensor_data.db;
 touch /usr/share/dht11res/dht11rs/sensor_data.db;
-# sudo systemctl daemon-reload;
-# sudo systemctl start dht11rs.service;
+sudo systemctl daemon-reload;
+sudo systemctl start dht11rs.service;
 
 
 
