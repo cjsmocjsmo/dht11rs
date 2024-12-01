7bb618537755e6ff498509f13f2417bec684ad53
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-12-01 03:25:58 -0800

    Fix directory path and update service script for dht11rs

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
 sudo systemctl stop dht11rs.service;
-cd /usr/share/dht11res/dht11res;
+cd /usr/share/dht11rs/dht11rs;
 git pull;
 cargo build --release;
-sudo cp -pvr /usr/share/dht11res/dht11res/target/release/dht11res /usr/bin/dht11res;
-rm /usr/share/dht11res/dht11rs/sensor_data.db;
-touch /usr/share/dht11res/dht11rs/sensor_data.db;
+sudo cp -pvr /usr/share/dht11rs/dht11rs/target/release/dht11rs /usr/bin/dht11rs;
+rm /usr/share/dht11rs/dht11rs/sensor_data.db;
+touch /usr/share/dht11rs/dht11rs/sensor_data.db;
 sudo systemctl daemon-reload;
 sudo systemctl start dht11rs.service;
 
